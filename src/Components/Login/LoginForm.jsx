7493b8f717d0128e24ce7603cf7b782386763847
Jorge Tomé <jorge.tomejr@outlook.com>
2024-02-27 19:31:32 -0400

    styles interface login

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -5,35 +5,35 @@ import { Button } from "../Forms/Button";
 import { UseForm } from "../../hooks/UseForm";
 import { UserContext } from "../../UserContext";
 import { Error } from "../Helper/Error";
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Forms/Button.module.css'
 
 export function LoginForm(){
     const username = UseForm()
     const password = UseForm()
-
     const {userLogin, error, loading} = useContext(UserContext)
-
    async function handleSubmit(e){
         e.preventDefault();
-
         if(username.validate() && password.validate()){
-            userLogin(username.value, password.value)
-            
+            userLogin(username.value, password.value)   
         }
     }
-
     return(
         <section className="animeleft">
             <h1 className="title">Login</h1>
-            <form onSubmit={handleSubmit}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <Input label="Usuário" type="text" name="username" {...username} />
                 <Input label="Senha" type="password" name="password" {...password}/>
-
                 {loading ? <Button disabled>Carregando...</Button>:
                 <Button>Enviar</Button>}
-                
                 {error && <Error error={error}/>}
             </form>
-            <Link to="/login/criar" >Cadastro</Link>
+            <Link className={styles.perdeu} to="/login/perdeu">Perdeu a senha</Link>
+            <div className={styles.cadastro}>
+                <h2 className={styles.subtitle} >Cadastre-se</h2>
+                <p>Ainda não possui conta ? Cadastre-se no site</p>
+                <Link className={stylesBtn.button} to="/login/criar" >Cadastro</Link>
+            </div>
         </section>
     )
 }
