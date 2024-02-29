3623595e753c0fcf2454ae085723f334f6f95bef
Jorge Tomé <jorge.tomejr@outlook.com>
2024-02-28 20:27:57 -0400

    create useFetch

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,9 +1,46 @@
 import { Link } from "react-router-dom";
+import { Input } from "../Forms/Input";
+import { Button } from "../Forms/Button";
+import { UseForm } from "../../hooks/UseForm";
+import { USER_POST } from "../../api";
+import { useContext } from "react";
+import { UserContext } from "../../UserContext";
+import { Error } from "../Helper/Error";
+import { UseFetch } from "../../hooks/UseFetch";
 
 export function LoginCreate(){
+    const username = UseForm()
+    const email = UseForm('email')
+    const password = UseForm()
+
+    const {userLogin} = useContext(UserContext)
+    const {error, loading, request} = UseFetch()
+
+   async function handleSubmit(e){
+        e.preventDefault()
+        if(username.validate() && email.validate() && password.validate()){
+            const {url, options} = USER_POST({
+                username: username.value,
+                email: email.value, 
+                password: password.value})
+            const {response, json} = await request(url, options)
+            if(response.ok){
+                await userLogin(username.value, password.value)
+            }
+        }
+    }
+
     return(
-        <div>
-            <Link></Link>
-        </div>
+        <section className="animeleft">
+            <h1 className="title">Cadastre-se</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="Usuário" type="text" name="username" {...username} />
+                <Input label="Email" type="email" name="email" {...email} />
+                <Input label="Senha" type="password" name="password" {...password} />
+                {loading ? <Button disabled>Carregando...</Button>:
+                <Button>Enviar</Button>}
+                {error && <Error error={error}/>}
+            </form>
+        </section>
     )
 }
--- src/hooks/UseForm.jsx
+++ src/hooks/UseForm.jsx
@@ -5,6 +5,11 @@ const types = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um E-mail válido', 
     },
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        message:
+          'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 número. Com no mínimo 8 caracteres.',
+      },
 }
 
 export function UseForm(type){
